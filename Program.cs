9171590338cadf803b281a4e8425d72d24f6c8df
yogesh <93078938+yogesh080@users.noreply.github.com>
2022-07-05 12:09:32 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,10 +2,7 @@
 {
     public class Program
     {
-        /// <summary>
-        /// This program is used to Add, create, remove,edit the Address Book of the Peoples.
-        /// </summary>
-        /// <param name="args"></param>
+        
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book Program");
